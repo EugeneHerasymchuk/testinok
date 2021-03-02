a3c7dca280b505a360aeab929b1d0afa3e24ca62
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2021-03-02 11:43:18 +0100

    Clean commented ElementUI lib config

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,29 +1,5 @@
-// import Vue from "vue";
-// import ElementUI from 'element-ui';
-
-import lang from "element-ui/lib/locale/lang/en";
 import locale from "element-ui/lib/locale";
+import lang from "element-ui/lib/locale/lang/en";
 
 locale.use(lang);
 
-// Vue.use(ElementUI);
-
-// Vue.use(Button);
-// Vue.use(Aside);
-// Vue.use(Main);
-// Vue.use(Footer);
-// Vue.use(Container);
-// Vue.use(Header);
-// Vue.use(Menu);
-// Vue.use(MenuItem);
-// Vue.use(Row);
-// Vue.use(Col);
-// Vue.use(Table);
-// Vue.use(TableColumn);
-// Vue.use(Tag);
-// Vue.use(Popconfirm);
-// Vue.use(Popover);
-// Vue.use(Dialog);
-
-
-// Vue.prototype.$message = Message;
