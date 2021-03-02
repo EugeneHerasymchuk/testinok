ebe8803f8ff42ab1a94d5699fe8df875391561f8
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2021-03-03 00:44:20 +0100

    Typo in error message of the apollo provider

--- src/plugins/vue-apollo.js
+++ src/plugins/vue-apollo.js
@@ -90,7 +90,7 @@ export function createProvider(options = {}) {
     errorHandler(error) {
       // eslint-disable-next-line no-console
       console.log(
-        "%cErrorżmine",
+        "%cError",
         "background: red; color: white; padding: 2px 4px; border-radius: 3px; font-weight: bold;",
         error.message
       );
