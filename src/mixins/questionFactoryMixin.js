973d0c6738c8b44f92322075d371eefc0cd16ba4
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2021-03-10 16:27:54 +0100

    Move components Map to shared mixin

--- src/mixins/questionFactoryMixin.js
+++ src/mixins/questionFactoryMixin.js
@@ -1,9 +1,18 @@
 import { QUESTION_OPTIONS, QUESTION_TYPES, Question } from "../constants/QuestionFactory";
+import RightOrderBuilder from "../components/Questions/RightOrder/RightOrderBuilder";
+import RightOrderPreview from "../components/Questions/RightOrder/RightOrderPreview";
+
 export default {
   data() {
     return {
       QUESTION_OPTIONS: QUESTION_OPTIONS,
-      QUESTION_TYPES: QUESTION_TYPES
+      QUESTION_TYPES: QUESTION_TYPES,
+      componentsMap: {
+        [QUESTION_TYPES.RightOrder]: {
+          builder: RightOrderBuilder,
+          preview: RightOrderPreview,
+        },
+      },
     };
   },
   methods: {
