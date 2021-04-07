b2d13b7683b93c3298212a832015db2c29705c91
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2021-04-08 00:28:16 +0200

    Reusable EditorPreview and RemoveAttachments button

--- src/components/Attachments/Editor/EditorPreview.vue
+++ src/components/Attachments/Editor/EditorPreview.vue
@@ -1,19 +1,8 @@
 <template>
-  <div class="editor">
-    <editor-content class="editor__content" :editor="editor" />
-  </div>
+  <EditorWrapper :attachmentConfig="attachmentConfig" :editable="false" />
 </template>
 <script>
-import { Editor, EditorContent } from "tiptap";
-import {
-  Bold,
-  Italic,
-  Strike,
-  Underline,
-  BulletList,
-  ListItem,
-  HorizontalRule,
-} from "tiptap-extensions";
+import EditorWrapper from "../components/EditorWrapper";
 
 export default {
   name: "EditorPreview",
@@ -24,36 +13,7 @@ export default {
     },
   },
   components: {
-    EditorContent,
-  },
-  data() {
-    return {
-      editor: new Editor({
-        extensions: [
-          new Bold(),
-          new Italic(),
-          new Strike(),
-          new Underline(),
-          new BulletList(),
-          new ListItem(),
-          new HorizontalRule(),
-        ],
-        content: this.attachmentConfig.content,
-        editable: false,
-      }),
-    };
-  },
-  beforeDestroy() {
-    this.editor.destroy();
+    EditorWrapper,
   },
 };
 </script>
-<style lang="sass" scoped>
-.editor
-  margin: 1rem 0
-  &__content
-    padding: .5rem 2rem
-    border: 1px solid #d3d4d6
-    border-radius: 6px
-    text-align: left
-</style>
---
+++ src/components/Attachments/components/RemoveAttachmentButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-popconfirm
+    confirm-button-text="Delete"
+    cancel-button-text="No"
+    icon="el-icon-info"
+    icon-color="red"
+    title="Are you sure to delete this attachment?"
+    @confirm="() => $emit('remove')"
+  >
+    <el-button
+      size="mini"
+      slot="reference"
+      type="danger"
+      icon="el-icon-delete"
+      circle
+    ></el-button>
+  </el-popconfirm>
+</template>
+<script>
+export default {
+  name: "RemoveAttachmentButton",
+};
+</script>
